89f5bab458e8f1057ccc316bd0d467d24eef2ea0
car2deerNrhino <kurimatu183@gmail.com>
2025-09-15 17:03:25 +0900

    Introduce Tokenizer

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,112 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// token kind
+typedef enum {
+  TK_RESERVED, // Symbol Token
+  TK_NUM,      // Integer Token
+  TK_EOF,      // End of Input Token
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;    // next input token
+  int val;        // if kind is TK_NUM, this is the number
+  char *str;      // token string
+};
+
+// currently focused token
+Token *token;
+
+// reporting errors
+// takes the same arguments as printf
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+/*
+// If the next token is the expected symol,
+// it reads one token and returns true,
+// otherwise it returns false.
+*/
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
+
+// The next token is the expected symbol, it read one token.
+// Otherwise it reports errors.
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c'ではありません", op);
+  token = token->next;
+}
+
+// The next token is a number, it reads one token and return the number.
+// Otherwise it reports errors.
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error("数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// Create a new token and connect it to cur.
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// Tokenizes the input string p and returns it.
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // Skip whitespace characters.
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,28 +114,27 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  token = tokenize(argv[1]);
 
+  // Output the first half of the assembly.
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf(" mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  // The first part of the expression must be a number,
+  // so check that and output the first mov instruction.
+  printf("  mov rax, %d\n", expect_number());
+
+  // Outputs assembly while consuming a sequence of tokens,
+  // `+ <number>` or `-<number>`.
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
   }
 
   printf("  ret\n");
